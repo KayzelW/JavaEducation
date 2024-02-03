6385001625ef70eac1a5307decbc3aff2b2a1322
Kayzel <kapitonovroman5@gmail.com>
2024-02-03 10:53:22 +0300

    Task Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        double a, b, c, x;
+        var input = new Scanner(System.in);
 
+        System.out.println("Введите a,b,c,x через пробел:");
+        a = input.nextDouble();
+        b = input.nextDouble();
+        c = input.nextDouble();
+        x = input.nextDouble();
+
+        System.out.println(Calculate(a, b, c, x));
+    }
+
+    private static double Calculate(double a, double b, double c, double x) {
+        double result;
+
+        result = Math.sqrt(
+                (a * Math.pow(x, 3) + Math.atan(x)) / (c * x + b * Math.abs(Math.log(x)))
+        );
+
+        return result;
     }
 }
